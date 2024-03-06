3a921bc6981f6f32e893f5602b0283fc6bb47566
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-05 19:52:17 -0500

    Impl() - Create Login Screen: Header

---
+++ app/src/main/java/com/dioses/jetpackcomposeinstagram/LoginScreen.kt
@@ -0,0 +1,41 @@
+package com.dioses.jetpackcomposeinstagram
+
+import android.app.Activity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+/****
+ * Project: JetpackComposeInstagram
+ * From: com.dioses.jetpackcomposeinstagram
+ * Created by Arthur Dioses Reto on 5/03/24 at 19:44
+ * All rights reserved 2024.
+ ****/
+@Composable
+fun LoginScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Header(Modifier.align(Alignment.TopEnd))
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "close app",
+        modifier = modifier.clickable { activity.finish() })
+}

046d4639748a986fae0b569e19bdf1d2a8d39e72
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-15 02:00:07 -0500

    Impl() - Data step 2

---
+++ app/src/main/java/com/dioses/jetpackcomposeinstagram/login/data/LoginRepository.kt
@@ -0,0 +1,16 @@
+package com.dioses.jetpackcomposeinstagram.login.data
+
+import com.dioses.jetpackcomposeinstagram.login.data.network.LoginService
+
+/****
+ * Project: JetpackComposeInstagram
+ * From: com.dioses.jetpackcomposeinstagram.login
+ * Created by Arthur Dioses Reto on 15/03/24 at 1:58 AM
+ * All rights reserved 2024.
+ ****/
+class LoginRepository {
+    private val api = LoginService()
+    suspend fun doLogin(user: String, password: String): Boolean {
+        return api.doLogin(user, password)
+    }
+}

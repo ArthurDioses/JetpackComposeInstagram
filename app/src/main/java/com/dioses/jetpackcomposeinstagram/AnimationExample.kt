c34024ca1af78466c706f82bab8dbca7fa907f61
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-18 00:03:44 -0500

    Impl() - Crossfade

--- app/src/main/java/com/dioses/jetpackcomposeinstagram/AnimationExample.kt
+++ app/src/main/java/com/dioses/jetpackcomposeinstagram/AnimationExample.kt
@@ -2,6 +2,7 @@ package com.dioses.jetpackcomposeinstagram
 
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
@@ -14,7 +15,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SensorDoor
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random.Default.nextInt
 
 /****
  * Project: JetpackComposeInstagram
@@ -100,4 +105,47 @@ fun VisibilityAnimation() {
             )
         }
     }
+}
+
+@Composable
+fun CrossfadeExampleAnimation() {
+    var myComponentType: ComponentType by remember {
+        mutableStateOf(ComponentType.Text)
+    }
+    Column(Modifier.fillMaxSize()) {
+        Button(onClick = { myComponentType = getComponentTypeRandom() }) {
+            Text(text = "Cambiar componente")
+
+        }
+        Crossfade(targetState = myComponentType) {
+            when (it) {
+                ComponentType.Image -> Icon(
+                    imageVector = Icons.Default.SensorDoor,
+                    contentDescription = ""
+                )
+
+                ComponentType.Text -> Text(text = "Soy un componente")
+                ComponentType.Box -> Box(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .background(Color.Red)
+                )
+
+                ComponentType.Error -> Text(text = "Erroooorrrr")
+            }
+        }
+    }
+}
+
+fun getComponentTypeRandom(): ComponentType {
+    return when (nextInt(from = 0, until = 3)) {
+        0 -> ComponentType.Image
+        1 -> ComponentType.Text
+        2 -> ComponentType.Box
+        else -> ComponentType.Error
+    }
+}
+
+enum class ComponentType() {
+    Image, Text, Box, Error
 }
